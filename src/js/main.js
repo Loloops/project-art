2b46443e313a65f6f91bdf1306d464c2b27c0ec0
V <sss.5151@mail.ru>
2021-09-13 18:36:11 +0400

    Всякие мелкие исправления + работа с отправкой форм

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 import modals from './modules/modal';
 import sliders from './modules/slider';
+import forms from './modules/forms';
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
   modals();
-  sliders('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn')
-  sliders('.main-slider-item', 'vertical')
+  sliders('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn');
+  sliders('.main-slider-item', 'vertical');
+  forms();
 });
