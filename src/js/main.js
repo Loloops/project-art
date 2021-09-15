f3898f5affb1af0976dae02e35cbe406892edf21
V <sss.5151@mail.ru>
2021-09-15 20:28:29 +0400

    Add smoothScroll to Block and Drag and Drop

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,8 @@ import filter from './modules/filter';
 import pictureSize from './modules/pictureSize'
 import accordion from './modules/accordion'
 import burger from './modules/burgerMenu'
+import scroll from './modules/scroll'
+import drop from './modules/dragDrop'
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
@@ -28,4 +30,6 @@ window.addEventListener('DOMContentLoaded', () => {
   pictureSize('.sizes-block')
   accordion('.accordion-heading')
   burger('.burger-menu', '.burger')
+  scroll('.pageup')
+  drop()
 });
